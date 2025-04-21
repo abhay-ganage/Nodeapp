9be13bf51040ca4f875fa959d9bb581bcc22faf6
abhi <abhi@.com>
2025-04-21 18:57:39 +0530

    nko

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
- 
+
 var app = express();
 
-app.get(‘/’, function (req, res) {
- res.send(‘hello Pune’);
+app.get('/', function (req, res) {
+  res.send('hello Pune');
 });
 
 app.listen(process.env.PORT || 80);
- 
+
 module.exports = app;
+
