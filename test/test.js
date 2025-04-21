276742ab77b6198da21bafe2558ebe55ea6ac7eb
abhi <abhi@.com>
2025-04-21 18:47:58 +0530

    hoil

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');describe('GET /', function() {
- it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
+ it('respond with hello world', function(done) { //navigate to roo>
  request(app).get('/').expect('hello world', done);
  });
 });
+
